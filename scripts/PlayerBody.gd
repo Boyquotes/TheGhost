0078bdf533268d68823a240eeb582dbb87ceeac6
Leonardo Ribeiro <leorr112624@gmail.com>
2022-10-04 22:10:24 -0300

    using blender model. planning pushing mechanic

--- scripts/PlayerBody.gd
+++ scripts/PlayerBody.gd
@@ -1,11 +1,23 @@
 extends RigidBody3D
 
+signal player_body_pos (position, delta)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var force: int = 5
+
+const DELTA = 0.01
+
+func _physics_process(delta):
+	emit_signal("player_body_pos", global_position, delta)
+
+func remove_not_moveable(obj : Node3D):
+	return obj.is_in_group("Moveable")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_player_motion_direction(direction):
+	var moveable_objects : Array[RigidBody3D] = get_colliding_bodies().filter(remove_not_moveable)
+	if moveable_objects:
+		for object in moveable_objects:
+			#TODO input para grabar o objeto
+				#mudar o estado para pushing
+				#minigame que criar uma linha entre o jogador e a bola, tem que imitar o movimento do empurrao, clicando e voltando atras das costas do player e empurrando com força na bola O_O
+			object.apply_central_impulse(direction*DELTA*force)
