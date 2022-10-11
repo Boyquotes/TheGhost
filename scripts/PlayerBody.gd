a3615c419f8e5585745cd19174fd43a21ce6518a
Leonardo Ribeiro <leorr112624@gmail.com>
2022-10-11 12:02:46 -0300

    sdasdasd

--- scripts/PlayerBody.gd
+++ scripts/PlayerBody.gd
@@ -12,12 +12,11 @@ func _physics_process(delta):
 func remove_not_moveable(obj : Node3D):
 	return obj.is_in_group("Moveable")
 
-
-func _on_player_motion_direction(direction):
+func _on_controller_motion_direction(direction):
 	var moveable_objects : Array[RigidBody3D] = get_colliding_bodies().filter(remove_not_moveable)
 	if moveable_objects:
 		for object in moveable_objects:
 			#TODO input para grabar o objeto
 				#mudar o estado para pushing
 				#minigame que criar uma linha entre o jogador e a bola, tem que imitar o movimento do empurrao, clicando e voltando atras das costas do player e empurrando com força na bola O_O
-			object.apply_central_impulse(direction*DELTA*force)
+			object.apply_central_impulse(direction*DELTA*force) # Replace with function body.
--- scripts/PlayerMesh.gd
+++ scripts/PlayerMesh.gd
@@ -2,11 +2,11 @@ extends Node3D
 
 @onready var animator : AnimationPlayer = get_node("AnimationPlayer")
 @export var material : Material
-@onready var mesh : MeshInstance3D = get_node("Armature/Skeleton3D/Cube")
+@onready var mesh : MeshInstance3D = get_node("Armature/Skeleton3D/Circle")
 
 func _on_sm_entered_state(state):
 	animator.play(state)
 
 func _ready():
-	if(material):
+	if(mesh):
 		mesh.set_surface_override_material(0, material)
