2892c547314d5248e8445a8e7204b7c39cd159cc
leorr <leorr112624@gmail.com>
2022-10-18 22:43:29 -0300

    2022/10/18 22:43

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,11 +4,11 @@ var motion : Vector3
 
 func move_to(destination):
 	if(destination):
-		motion = destination - global_position
-		velocity = (destination - global_position).normalized() * 5
-		move_and_slide()
+		#motion = destination - global_position
+		#velocity = (destination - global_position).normalized() * 5
+		#move_and_slide()
 		#if (fmod(rad_to_deg(atan2(-motion.x, -motion.z) - rotation.y), 360) < 10):
-		#	pass
+			pass
 
 func _physics_process(delta):
 	rotation.y = lerp_angle(rotation.y, atan2(-motion.x, -motion.z), delta)
