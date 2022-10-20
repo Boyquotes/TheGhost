a2347e7583496e875dba8281202b01d0826479bb
Leonardo Ribeiro <leorr112624@gmail.com>
2022-10-19 23:08:43 -0300

    dsadsadasd

--- scripts/PlayerMesh.gd
+++ scripts/PlayerMesh.gd
@@ -4,7 +4,7 @@ extends Node3D
 @onready var veil : MeshInstance3D = get_node("Armature/Skeleton3D/Veil")
 @onready var glove : MeshInstance3D = get_node("Armature/Skeleton3D/Gloves")
 @onready var veilMaterial : BaseMaterial3D = preload("res://materials/veilMaterial.tres")
-@onready var gloveMaterial : BaseMaterial3D = preload("res://materials/gloveMaterial.tres")
+@onready var gloveMaterial : BaseMaterial3D = preload("res://materials/veilMaterial.tres")
 
 func _on_sm_entered_state(state):
 	animator.play(state)
